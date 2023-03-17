a77614e68a0b4fcae37fb8b63269b7d3c2766ad1
VoidyCD <pvadiscordteam@gmail.com>
2023-03-17 11:48:35 +0300

    devTools

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 const path = require('path')
 const { app, BrowserWindow } = require('electron')
 
+const isDev = process.env.NODE_EV !== 'production'
 const isMac = process.platform === 'darwin'
+
 
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'Image Reizer',
-        width: 500,
+        width: isDev ? 1000: 500,
         height: 600
     });
+
+    //* Open Dev tools if in developer enviroment
+    if(isDev) {
+        mainWindow.webContents.openDevTools();
+    }
 
     mainWindow.loadFile(path.join(__dirname, './renderer/index.html')) ;
 }
