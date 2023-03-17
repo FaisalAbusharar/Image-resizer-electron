28b463e0b5f1b2fc24be3f9a2c22b820f117eea2
VoidyCD <pvadiscordteam@gmail.com>
2023-03-17 12:04:47 +0300

    added Menu template

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const path = require('path')
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 
 const isDev = process.env.NODE_EV !== 'production'
 const isMac = process.platform === 'darwin'
 
-
+//* Create main window
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'Image Reizer',
@@ -20,8 +20,14 @@ function createMainWindow() {
     mainWindow.loadFile(path.join(__dirname, './renderer/index.html')) ;
 }
 
+//* App is ready
+
 app.whenReady().then(() => {
     createMainWindow();
+
+    //! Implment menu
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu)
 
     app.on('activate', () =>  {
         if (BrowserWindow.getAllWindows().length === 0) {
@@ -29,6 +35,19 @@ app.whenReady().then(() => {
         }
     });
 });
+
+
+//* Menu template
+const menu = [
+    {
+        label: 'File',
+        submenu: [{
+            label: 'Quit',
+            click: () => app.quit(),
+            accelerator: "Alt+F4"
+        }]
+    }
+]
 
 app.on('window-all-closed', () => {
     if (!isMac) {
